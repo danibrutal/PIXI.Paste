58d362a3b7b0ffb9f3483da0693b59e55a95da9c
Daniel Martinez <danimartcas@hotmail.com>
2021-11-12 20:49:33 +0100

    Trying to export ts

---
+++ src/index.ts
@@ -0,0 +1,67 @@
+
+import { Container } from "@pixi/display";
+import { Texture } from "@pixi/core";
+import { Sprite } from "@pixi/sprite";
+import { Text } from "@pixi/text";
+
+
+declare global {
+  interface Window {
+    clipboardData: DataTransfer;
+  }
+}
+
+const isImage = (item: DataTransferItem) => (item.kind == 'file' && item.type.match('^image/'));
+const isPlainText = (item: DataTransferItem) => (item.kind == 'string' && item.type.match('^text/plain$'));
+
+export default class PasteContainer extends Container {
+  constructor(parent: Container) {
+    super();
+
+    this.setParent(parent);
+    window.document.onpaste = (evt: ClipboardEvent) => {
+      const dT = evt.clipboardData || window.clipboardData as DataTransfer;
+      const items = dT.items;
+  
+      for (let index in items) {
+        if (isImage(items[index])) {
+          this.handleImage(items[index]);
+        }else if(isPlainText(items[index])) {
+          this.handlePlainText(items[index]);
+        }
+      }
+    }
+  }
+
+  handlePlainText (item: DataTransferItem) {
+    item.getAsString((string) => {
+      let text = new Text(string);
+
+      this.addChild(text);
+    });
+  }
+
+  handleImage (item: DataTransferItem) {
+    const file = item.getAsFile();
+    
+    if (null === file) {
+      return;
+    }
+
+    const reader: FileReader  = new FileReader();
+  
+    reader.onload = (e) =>  {
+      const image: HTMLImageElement = document.createElement("img");
+      image.src = reader.result as string;
+      
+      const texture = Texture.from(image);
+      const sprite = new Sprite(texture);
+      sprite.x = Math.random() * (this.parent.width * 0.7);
+      sprite.y = Math.random() * (this.parent.height * 0.7);
+
+      this.addChild(sprite);
+    }
+  
+    reader.readAsDataURL(file as Blob);
+  };
+}
